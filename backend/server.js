ddc2da1f435b155f9ccf2d5caa8ab70578b14ba0
Nitin Kumar <ng6397100896@gmail.com>
2025-09-03 19:29:38 +0530

    reder deployment changes

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { dbconnect } from './config/database.js'
+import cloudinaryConnect from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+// app config
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connect DB & Cloudinary
+dbconnect();
+cloudinaryConnect();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoint
+app.get('/', (req, res) => {
+  res.send("API Working...");
+});
+
+app.use('/api/admin', adminRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
+
+// listen for requests
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
